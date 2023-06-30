ec9d648a9c114ed28b74f7db0c6ff5c5fdde49dc
thesidorenko <sidorennko21@gmail.com>
2023-06-30 14:01:22 +0300

    Added sorting in the table

--- components/src/pages/TablePage.js
+++ components/src/pages/TablePage.js
@@ -1,33 +1,37 @@
-import Table from '../components/Table'
+import SortableTable from '../components/SortableTable'
 
 function TablePage() {
   const data = [
-    {name: 'Orange', color: 'bg-orange-500', score: 7},
-    {name: 'Lime', color: 'bg-green-500', score: 1},
-    {name: 'Apple', color: 'bg-red-500', score: 3},
-    {name: 'Banana', color: 'bg-yellow-500', score: 4}
+    { name: 'Orange', color: 'bg-orange-500', score: 7 },
+    { name: 'Lime', color: 'bg-green-500', score: 1 },
+    { name: 'Apple', color: 'bg-red-500', score: 3 },
+    { name: 'Banana', color: 'bg-yellow-500', score: 4 },
+    { name: 'Cherry', color: 'bg-red-800', score: 9 },
   ]
 
   const config = [
     {
       label: 'Fruit',
-      render: fruit => fruit.name
+      render: fruit => fruit.name,
+      sortValue: fruit => fruit.name,
     },
     {
       label: 'Color',
-      render: fruit => <div className={`p-3 m-2 opacity-70 border border-current ${fruit.color}`} />
+      render: fruit => <div className={`p-3 m-2 opacity-80 ${fruit.color}`} />,
     },
     {
       label: 'Score',
       render: fruit => fruit.score,
-      header: () =><th className='bg-green-400'>Score</th>
-    }
+      sortValue: fruit => fruit.score,
+    },
   ]
 
-  const keyFn = (fruit) => fruit.name
+  const keyFn = fruit => fruit.name
 
   return (
-    <div><Table data={data} config={config} keyFn={keyFn} /></div>
+    <div>
+      <SortableTable data={data} config={config} keyFn={keyFn} />
+    </div>
   )
 }
 
